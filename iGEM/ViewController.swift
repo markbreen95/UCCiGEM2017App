4f78697283098534b6e9662e9cb8ff1b613fa378
Ross Hill <Ross@Rosss-MacBook-Pro-2.local>
2017-08-31 15:51:15 +0100

    Initial Commit

---
+++ iGEM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iGEM
+//
+//  Created by Ross Hill on 31/08/2017.
+//  Copyright © 2017 UCC iGEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iGEMTests/iGEMTests.swift
@@ -0,0 +1,36 @@
+//
+//  iGEMTests.swift
+//  iGEMTests
+//
+//  Created by Ross Hill on 31/08/2017.
+//  Copyright © 2017 UCC iGEM. All rights reserved.
+//
+
+import XCTest
+@testable import iGEM
+
+class iGEMTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
